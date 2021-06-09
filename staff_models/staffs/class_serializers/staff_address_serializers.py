993fd6e1a081f077a7f37ae2a7ad263b3db14d18
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-09 12:51:02 +0700

    .

--- staff_models/staffs/class_serializers/staff_address_serializers.py
+++ staff_models/staffs/class_serializers/staff_address_serializers.py
@@ -1,11 +1,16 @@
 from rest_flex_fields import FlexFieldsModelSerializer
 
-
 # product size serializer
+from staff_models.staffs.class_models.staff import Staff
 from staff_models.staffs.class_models.staff_address import StaffAddress
+from rest_framework_utils.key_related_field import key_related_field
 
 
 class StaffAddressSerializer(FlexFieldsModelSerializer):
+    # staff = key_related_field.related_field(
+    #     Staff.objects.filter(is_active=True)
+    # )
+
     class Meta:
         model = StaffAddress
         fields = ['id', 'url', 'staff', 'address', 'map', 'city', 'country']
--- staff_models/staffs/class_serializers/staff_phone_serializers.py
+++ staff_models/staffs/class_serializers/staff_phone_serializers.py
@@ -1,10 +1,17 @@
 from rest_flex_fields import FlexFieldsModelSerializer
 
 # product size serializer
+from rest_framework_utils.key_related_field import key_related_field
+
+from staff_models.staffs.class_models.staff import Staff
 from staff_models.staffs.class_models.staff_phone import StaffPhone
 
 
 class StaffPhoneSerializer(FlexFieldsModelSerializer):
+    # staff = key_related_field.related_field(
+    #     Staff.objects.filter(is_active=True)
+    # )
+
     class Meta:
         model = StaffPhone
         fields = ['id', 'url', 'staff', 'phone']
